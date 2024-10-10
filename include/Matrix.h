aae9ffea7c02c21ad12494fe2abdae55445c17df
Vivek <138117731+g-dsservant@users.noreply.github.com>
2024-10-10 11:42:24 +0530

    Update Matrix.h

--- include/Matrix.h
+++ include/Matrix.h
@@ -1,3 +1,4 @@
+
 #ifndef MATRIX
 #define MATRIX
 
@@ -5,6 +6,7 @@ typedef struct Matrix Matrix;
 
 
 Matrix* Matrix_create(int rows, int cols);
+Matrix* Matrix_fromArray(int rows, int cols, double *arr);
 void Matrix_free(Matrix **A);
 void Matrix_freeN(int n, ...);
 
@@ -13,13 +15,16 @@ void Matrix_print(Matrix *A, const char *name);
 void Matrix_fill(Matrix *A, double num);
 void Matrix_rand(Matrix *A, int low, int high);
 
+int colsize(Matrix *A);
+int rowsize(Matrix *A);
+
 void Matrix_sum(Matrix *A, Matrix *B);
 void Matrix_multiply(Matrix *Result, Matrix *A, Matrix *B);
 void Matrix_difference(Matrix *A, Matrix *B);
 void Matrix_haddamard(Matrix *A, Matrix *B);
 void Matrix_scalarprod(Matrix *A, double B);
 
-void Matrix_transpose(Matrix *A);
+Matrix* Matrix_transpose(Matrix *A);
 Matrix* Matrix_submatrix(Matrix *A, int row_stride, int col_stride);
 Matrix* Matrix_slice(Matrix* A, int row_start, int row_end, int col_start, int col_end);
 Matrix* Matrix_resize(Matrix *A, int rows, int cols);

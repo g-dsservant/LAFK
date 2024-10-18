41b75cef2840619d6909463e86b2c0351325e2fc
Vivek <138117731+g-dsservant@users.noreply.github.com>
2024-10-18 19:46:31 +0530

    Update Matrix_private.h

--- src/Matrix_private.h
+++ src/Matrix_private.h
@@ -3,10 +3,12 @@
 #define MATRIX_PRIVATE
 
 #include "Matrix.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-#define CHECK(A) assert(A != NULL)
-#define MAT_POS(m, i, j) (m->elements)[i*(m->row_stride) + j*(m->col_stride)]
-#define FLAT_INDEX(m, i, j) (i*(m->row_stride) + j*(m->col_stride))
+#define MAT_POS(m, i, j) (m->elements)[(i-1)*(m->row_stride) + (j-1)*(m->col_stride)]
+#define FLAT_INDEX(m, i, j) ((i-1)*(m->row_stride) + (j-1)*(m->col_stride))
+
 
 struct Matrix {
   int rows;
@@ -17,6 +19,16 @@ struct Matrix {
   int memfree_flag;
 };
 
+
+void Swap(void* a, void* b, size_t size);
+
+
+static inline void CHECK(int condition, const char* message) {
+  if (!condition) {
+    fprintf(stderr, "%s", message);
+    exit(1);
+  }
+}
 
 
 #endif

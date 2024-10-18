f4651ff9a99b39d287fc2dff4a27f5f7eff7b13f
Vivek <138117731+g-dsservant@users.noreply.github.com>
2024-10-18 19:45:24 +0530

    Update Matrix.h

--- include/Matrix.h
+++ include/Matrix.h
@@ -1,4 +1,3 @@
-
 #ifndef MATRIX
 #define MATRIX
 
@@ -19,14 +18,21 @@ int colsize(Matrix *A);
 int rowsize(Matrix *A);
 
 void Matrix_sum(Matrix *A, Matrix *B);
+Matrix* Matrix_sum_new(Matrix *A, Matrix *B);
 void Matrix_multiply(Matrix *Result, Matrix *A, Matrix *B);
+Matrix* Matrix_multiply_new(Matrix *A, Matrix *B);
 void Matrix_difference(Matrix *A, Matrix *B);
+Matrix* Matrix_difference_new(Matrix *A, Matrix *B);
 void Matrix_haddamard(Matrix *A, Matrix *B);
+Matrix* Matrix_haddamard_new(Matrix *A, Matrix *B);
 void Matrix_scalarprod(Matrix *A, double B);
+Matrix* Matrix_scalarprod_new(Matrix *A, double B);
 
 Matrix* Matrix_transpose(Matrix *A);
+Matrix* Matrix_transpose_new(Matrix *A);
 Matrix* Matrix_submatrix(Matrix *A, int row_stride, int col_stride);
 Matrix* Matrix_slice(Matrix* A, int row_start, int row_end, int col_start, int col_end);
+
 Matrix* Matrix_resize(Matrix *A, int rows, int cols);
 
 #endif
